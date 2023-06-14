f0f88d467d650bad116053cd18ec051ac7c8f75a
eduardobnhr <eduardobnhr@gmail.com>
2023-06-14 09:25:00 -0400

    ChessExcpetion and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,8 +6,6 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("teste");
-		System.out.println("relative");
 		ChessMatch chessMacth = new ChessMatch();
 		UI.printBoard(chessMacth.getPieces());
 		
