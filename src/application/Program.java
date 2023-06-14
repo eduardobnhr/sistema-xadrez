530a53d3e87246a3fda5bf82cdccad76d6507cd0
eduardobnhr <eduardobnhr@gmail.com>
2023-06-14 08:55:03 -0400

    acrescentando uma linha

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("teste");
+		System.out.println("relative");
 		ChessMatch chessMacth = new ChessMatch();
 		UI.printBoard(chessMacth.getPieces());
 		
