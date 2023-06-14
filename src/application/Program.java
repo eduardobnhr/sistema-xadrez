d4694934d76619e6945b219a143626631988490e
eduardobnhr <eduardobnhr@gmail.com>
2023-06-14 07:49:57 -0400

    testando alteracoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println("teste");
 		ChessMatch chessMacth = new ChessMatch();
 		UI.printBoard(chessMacth.getPieces());
 		
