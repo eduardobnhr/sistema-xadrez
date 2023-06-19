9ab81b52ac8c052eeea5a3814e53020165df37eb
eduardobnhr <eduardobnhr@gmail.com>
2023-06-19 10:15:53 -0400

    moving pieces/movendo pecas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMacth = new ChessMatch();
-		UI.printBoard(chessMacth.getPieces());
 		
+		while(true) {
+			UI.printBoard(chessMacth.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = ChessMatch(source, target);
+		}
+			
+	}
+
+	private static ChessPiece ChessMatch(ChessPosition source, ChessPosition target) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
